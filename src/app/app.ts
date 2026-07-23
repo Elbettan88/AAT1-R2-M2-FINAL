d703f390b30874769502387131a3951f24faa9f0
Luis Javier Betancourth Morales <luisbetancourth88@gmail.com>
2026-07-22 23:48:18 -0600

    feat: footer completado con estilos alineados al navbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Navbar} from './components/navbar/navbar';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar], // <--- Cambiamos 'NavbarComponent' por 'Navbar'
+  imports: [RouterOutlet, Navbar, Footer], // <--- Cambiamos 'NavbarComponent' por 'Navbar'
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
