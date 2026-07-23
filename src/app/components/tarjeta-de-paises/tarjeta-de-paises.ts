859d71ada43920bbae4a68ba83941768cc08a248
KerenRey <reynosokeren99@gmail.com>
2026-07-22 18:09:57 -0600

    Corregir src/app

---
+++ src/app/components/tarjeta-de-paises/tarjeta-de-paises.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tarjeta-de-paises',
+  imports: [],
+  templateUrl: './tarjeta-de-paises.html',
+  styleUrl: './tarjeta-de-paises.css',
+})
+export class TarjetaDePaises {}
